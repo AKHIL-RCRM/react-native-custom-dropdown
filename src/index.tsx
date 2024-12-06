613a3737a4227d93283afd14fc020d7cffe20d65
Mondu Akhil <akhilm@recruitcrm.io>
2024-12-06 20:14:07 +0530

    test2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import Dropdown from './components/Dropdown';
 import MultiSelect from './components/MultiSelect';
 import SelectCountry from './components/SelectCountry';
+import DropDownWithDownIcon from './components/DropDownWithDownIcon';
 import { IDropdownRef } from './components/Dropdown/model';
 import { IMultiSelectRef } from './components/MultiSelect/model';
 
-export { Dropdown, MultiSelect, SelectCountry, IDropdownRef, IMultiSelectRef };
+export { Dropdown, MultiSelect, SelectCountry, DropDownWithDownIcon, IDropdownRef, IMultiSelectRef };
