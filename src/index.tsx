ae8acf7c49852e352b4c4db5fcfd729403672868
Mondu Akhil <akhilm@recruitcrm.io>
2024-12-08 19:45:28 +0530

    added component for from number dropdown

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import Dropdown from './components/Dropdown';
 import MultiSelect from './components/MultiSelect';
 import SelectCountry from './components/SelectCountry';
+import DropDownWithDownIcon from './components/DropDownWithDownIcon';
+import SelectNumberDropdown from './components/SelectNumberDropdown';
 import { IDropdownRef } from './components/Dropdown/model';
 import { IMultiSelectRef } from './components/MultiSelect/model';
 
-export { Dropdown, MultiSelect, SelectCountry, IDropdownRef, IMultiSelectRef };
+export { Dropdown, MultiSelect, SelectCountry, DropDownWithDownIcon, SelectNumberDropdown, IDropdownRef, IMultiSelectRef };
